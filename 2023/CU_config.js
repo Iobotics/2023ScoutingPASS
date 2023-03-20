504e3f46f418f441afba4f5161997929267c1e15
stacezhao <59520267+stacezhao@users.noreply.github.com>
2023-03-20 09:38:47 -1000

    Canadian Pacific

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023azva",
+      "defaultValue": "2023bcvi",
       "required": "true",
       "disabled": "true"
     },
