b5abd89cb71fbc707aa430e92df1decfa445c5ff
Shaei1 <127363334+Shaei1@users.noreply.github.com>
2023-04-05 22:58:18 -1000

    Tallahassee Regional

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023lake",
+      "defaultValue": "2023flta",
       "required": "true",
       "disabled": "true"
     },
